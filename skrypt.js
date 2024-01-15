5f848c6f638b6cebb8e6ec950c1817ab3c30e175
Dziadzior <patrykroszkowski2020@gmail.com>
2024-01-15 16:32:37 +0100

    2

--- skrypt.js
+++ skrypt.js
@@ -1,8 +1,15 @@
 const plikPole = document.querySelector('.plik-pole'),
-    filtrOpcje = document.querySelector('.filtr button'),
+    filtrOpcje = document.querySelectorAll('.filtr button'),
+    filtrNazwa = document.querySelector('.filtr-info .nazwa'),
+    filtrWartosc = document.querySelector('.pasek .wartosc'),
+    filtrPasek = document.querySelector('.pasek input'),
     podgladZdjecia = document.querySelector('.podglad-zdjecia img'),
     wybierzZdjeciePrzycisk = document.querySelector('.wybierz-zdjecie');
 
+let jasnosc = 100,
+    saturacja = 100,
+    inwersja = 0,
+    skala = 0;
 
 const zaladujZdjecie = () => {
     let plik = plikPole.files[0];
@@ -18,9 +25,25 @@ filtrOpcje.forEach(opcja => {
     opcja.addEventListener('click', () => {
         document.querySelector('.filtr .aktywny').classList.remove('aktywny');
         opcja.classList.add('aktywny');
+        filtrNazwa.innerText = opcja.innerText;
     });
 });
 
-plikPole.addEventListener('change', zaladujZdjecie);
+const aktualizacjaFiltr = () => {
+    filtrWartosc.innerText = `${filtrPasek.value}%`;
+    const wybranyFiltr = document.querySelector('.filtr .aktywny');
 
+    if (wybranyFiltr.id === 'jasnosc') {
+        jasnosc = filtrPasek.value;
+    } else if(wybranyFiltr.id === 'saturacja') {
+        saturacja = filtrPasek.value;
+    } else if(wybranyFiltr.id === 'inwersja') {
+        inwersja = filtrPasek.value;
+    } else {
+        skala = filtrPasek.value;
+    }
+}
+
+plikPole.addEventListener('change', zaladujZdjecie);
+filtrPasek.addEventListener('input', aktualizacjaFiltr);
 wybierzZdjeciePrzycisk.addEventListener('click', () => plikPole.click());
